868c386c9306b5bc9fe5a98b966e3690c3ffefec
Jeonghoon2 <ukidd12@naver.com>
2024-11-05 19:44:17 +0900

    git init

---
+++ src/main/java/com/jeong/tripcok/TripCokApplication.java
@@ -0,0 +1,13 @@
+package com.jeong.tripcok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripCokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeong/tripcok/TripCokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeong.tripcok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
